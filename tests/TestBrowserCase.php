f049eb1dcae32f5356cb18095cfdfdd8f2c75fab
Alexandr Chernyaev <bliz48rus@gmail.com>
2024-09-27 02:08:37 +0300

    test

--- tests/TestBrowserCase.php
+++ tests/TestBrowserCase.php
@@ -48,6 +48,10 @@ abstract class TestBrowserCase extends TestCase
         ]);
 
         config()->set('platform.prevents_abandonment', false);
+
+        if (isset($_SERVER['CI'])) {
+            Options::withoutUI();
+        }
     }
 
     protected function createAdminUser(array $attributes = []): User
